1ce319cfca309a078c25255a3b758e4acc14d3de
Facundo Jimenez <facundo.gabriel.jimenez@gmail.com>
2021-05-01 13:08:28 -0300

    cambio remitente form

--- email.php
+++ email.php
@@ -15,15 +15,15 @@ try {
     //Server settings
     $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'c2191146.ferozo.com';                     //Set the SMTP server to send through
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'test@c2191146.ferozo.com';                     //SMTP username
-    $mail->Password   = 'Camada@15810';                         //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-    $mail->Port       = 465;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+    $mail->Username   = 'facu.jimenez.cavs@gmail.com';                     //SMTP username
+    $mail->Password   = 'Facu2000';                         //SMTP password
+    $mail->SMTPSecure = 'tls';         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
     //Recipients
-    $mail->setFrom('test@c2191146.ferozo.com', 'Camada 15810'); // Hacer coincidir con el username. (preferentemente)
+    $mail->setFrom('facu.jimenez.cavs@gmail.com', 'Facundo'); // Hacer coincidir con el username. (preferentemente)
     $mail->addAddress('facu.jimenez.cavs@gmail.com', 'Facundo');     //Add a recipient
     //$mail->addAddress('ellen@example.com');               //Name is optional
     //$mail->addReplyTo('info@example.com', 'Information');
